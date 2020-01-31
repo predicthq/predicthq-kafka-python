e1d19749760f3b6d7e9f3b19b56a075b021b442d
driadi <denny@predicthq.com>
2020-01-31 15:39:58 +1300

    Fix issue related to Prometheus metrics.

--- phq/kafka/metrics.py
+++ phq/kafka/metrics.py
@@ -1,9 +1,26 @@
 from prometheus_client import Counter, Summary
 
+CONSUME_TIME_METRIC = None
+CONSUME_COUNT_METRIC = None
+
 
 def consumer_time_metric(svc_name):
-    return Summary(f'{svc_name}_kafka_process_seconds', 'Time spent processing Kafka messages')
+    # Prometheus library throws "Duplicated {metric} in CollectorRegistry" exception when
+    # metric object is re-created with the same name, hence we need to keep reusing the same one.
+    global CONSUME_TIME_METRIC
+
+    if not CONSUME_TIME_METRIC:
+        CONSUME_TIME_METRIC = Summary(f'{svc_name}_kafka_process_seconds', 'Time spent processing Kafka messages')
+
+    return CONSUME_TIME_METRIC
 
 
 def consumer_count_metric(svc_name):
-    return Counter(f'{svc_name}_kafka_process_count', 'Count of Kafka messages processed')
+    # Prometheus library throws "Duplicated {metric} in CollectorRegistry" exception when
+    # metric object is re-created with the same name, hence we need to keep reusing the same one.
+    global CONSUME_COUNT_METRIC
+
+    if not CONSUME_COUNT_METRIC:
+        CONSUME_COUNT_METRIC = Counter(f'{svc_name}_kafka_process_count', 'Count of Kafka messages processed')
+
+    return CONSUME_COUNT_METRIC
